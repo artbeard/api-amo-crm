7662c961c2d8fb2b9d2548d5c9c93830c28865a5
artbeard <76746834+artbeard@users.noreply.github.com>
2023-02-18 18:59:57 +0600

    Созданы все три компонента

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,34 @@
 <template>
 	<div>
 		<div class="form-row">
-			<Dropdown class="form-input mr-1" v-model="typeResource" :data="selectionList"></Dropdown>
-			<button
-				:disabled="typeResource === null"
-				@click="careateResotce()"
-				class="btn">
-				<i class="spinner" v-if="requestAnimation"></i>
-				<template v-else>
-					Создать
-				</template>
-			</button>
+			<Dropdown
+				class="form-input mr-1"
+				v-model="typeResource"
+				:data="selectionList"
+				></Dropdown>
+			<Button
+				:disable="disableButton"
+				:animate="isRequest"
+				@click="createResource">
+				Создать
+			</Button>
 		</div>
-		<div class="form-row" v-if="validStatus">
-			<div class="notice error">{{ validStatus }}</div>
+		<div class="form-row" v-if="validationMessage">
+			<div class="notice error">{{ validationMessage }}</div>
 		</div>
-
+		<ListEntity :columns="columns" :entity-list="listEntity"></ListEntity>
 	</div>
 </template>
 
 <script setup lang="ts">
-import Dropdown from './components/Dropdown.vue';
-import { ref, reactive } from 'vue';
+import Dropdown, { IListSelectable, OptionValue } from './components/Dropdown.vue';
+import Button from './components/Button.vue';
+import ListEntity, { IColumns, IEntity } from './components/ListEntity.vue'
+import { ref, reactive, computed, markRaw } from 'vue';
 /**
  * Тип создаваемго ресурса
  */
-const typeResource = ref<string|number|null>(null);
+const typeResource = ref<OptionValue>(null);
 /**
  * Варианты создаваемого ресурса
  */
@@ -34,7 +37,11 @@ const typeResource = ref<string|number|null>(null);
 	Contact,
   	Company,
 }
-const selectionList = reactive<IListSelectable[]>([
+/**
+ * простой список для выбора создаваемых сущностей,
+ * реактивность не нужна
+ */
+const selectionList = markRaw<IListSelectable[]>([
 	{
 		value: ResTypes.Deal,
 		label: 'Сделка'
@@ -49,17 +56,54 @@ const selectionList = reactive<IListSelectable[]>([
 	},
 ]);
 
-//ключ для api 30878566
-
-const requestAnimation = ref<boolean>(false);
-const validStatus = ref<string|null>(null);
-const careateResotce = function(){
-	validStatus.value = null;
-	if (typeResource === null)
+/**
+ * колонки для таблицы, которя будет выводить созданные сущности сущности
+ */
+const columns: IColumns = markRaw({
+	id: 'Id Созданной сущности',
+	title: 'Название созданной сущности',
+})
+//временное хранение моковых данных
+const listEntity:IEntity[] = reactive([
 	{
-		validStatus.value = 'Вы не выбрали тип создаваемого ресурса';
+		id: 153, title: '1-я сущность'
+	},
+	{
+		id: 154, title: '2-я сущность'
+	},
+	{
+		id: 155, title: '3-я сущность'
+	},
+
+]);
+
+/**
+ * Флаг выполнения запроса (для анимации)
+ */
+const isRequest = ref<boolean>(false);
+/**
+ * активация/деактивация кнопки 
+ */
+const disableButton = computed(()=>{
+	return typeResource.value === null;
+})
+/**
+ * сообщение об ошибке
+ */
+const validationMessage = ref<string|null>(null);
+/**
+ * Функция создания сущности
+ */
+const createResource = function(){
+	//Если запрос уже выполняется, ждем его окончания
+	if (isRequest.value)
+		return;
+	validationMessage.value = null;
+	if (typeResource.value === null)
+	{
+		validationMessage.value = 'Вы не выбрали тип создаваемого ресурса';
 		return;
 	}
-	requestAnimation.value = true;
+	isRequest.value = true;
 };
 </script>
